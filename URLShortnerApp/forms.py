d1e6eb127ec724a64fabe0bc41e2b81326aa4387
Prabhpreet <prabhpreet1601@gmail.com>
2020-08-04 19:03:20 +0530

    learning merge conflicts

--- URLShortnerApp/forms.py
+++ URLShortnerApp/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class URLDataForm(forms.Form):
-	EnterURL=forms.CharField(label='Enter U"r URL ', max_length=1000, widget=forms.TextInput(attrs={'placeholder': 'Shorten URL Here'}))
+	EnterURL=forms.CharField(label='Enter Yor URL: ', max_length=1000, widget=forms.TextInput(attrs={'placeholder': 'Shorten URL Here'}))
