0fa29315b7ef67bf7d81cdae5c5b8f2a92f72e90
prabhpreet7 <48807249+prabhpreet7@users.noreply.github.com>
2020-08-05 13:45:21 +0530

    Double please

--- URLShortnerApp/forms.py
+++ URLShortnerApp/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class URLDataForm(forms.Form):
-	EnterURL=forms.CharField(label='Enter Your URL Please: ', max_length=1000, widget=forms.TextInput(attrs={'placeholder': 'Shorten URL Here'}))
+	EnterURL=forms.CharField(label='Enter Your URL Please PLEASE: ', max_length=1000, widget=forms.TextInput(attrs={'placeholder': 'Shorten URL Here'}))
